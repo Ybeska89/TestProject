9f23b76459b6d14401f82d8823fb8b3be80e0809
Yasin beskardes <yaso.kardes@gmail.com>
2020-10-21 10:49:43 -0500

    fixed

--- InterviewPractices/src/test/java/lovevery/pages/LoveveryBlockSetPage.java
+++ InterviewPractices/src/test/java/lovevery/pages/LoveveryBlockSetPage.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import lovevery.utilities.CommonMethods;
 import lovevery.utilities.LoveveryDriver;
